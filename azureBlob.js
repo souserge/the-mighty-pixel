28481033fb0ea5341f06383cba380fd8757913e0
Serge Korzh <serge.korzh@gmail.com>
2017-07-09 10:01:04 +0300

    Create GridStorageManager class

--- azureBlob.js
+++ azureBlob.js
@@ -3,15 +3,63 @@ const azure = require('azure')
 
 const config = {
   connectionString: 'local',
-  blobContainerName: 'gridcontainer'
+  blobContainerName: 'grid-container',
+  gridMapBlobName: 'grid-map',
+  gridMetadataBlobName: 'grid-metadata'
 }
 
+class GridStorageManager {
+  constructor(errorCallback) {
+    this.errorCallback = errorCallback
+    this.blobService = azure.createBlobService(config.connectionString)
+    this.isContainerCreated = false
 
-const blobService = azure.createBlobService(config.connectionString)
+    this.blobService.createContainerIfNotExists(config.blobContainerName,
+      (error, result, response) => {
+      if (error) { this.errorCallback(error) }
+      else { this.isContainerCreated = true }
+    })
+  }
 
-blobService.createContainerIfNotExists(config.blobContainerName,
-  (error, result, response) => {
-  if (error) return false
+  upload(grid) {
+    if (!this.isContainerCreated) return
 
-  // Read/Write grid-map-keys, grid-map-values and grid-metadata
-})
+    //TODO: do something with grid
+    gridMapJSON = '{}'
+    gridMetadataJSON = '{}'
+
+    this.blobService.createBlockBlobFromText(config.blobContainerName,
+      config.gridMapBlobName, gridMapJSON, (error, result, response) => {
+      if(error) { this.errorCallback(error) }
+      else { /* gridMap uploaded */ }
+    })
+
+    this.blobService.createBlockBlobFromText(config.blobContainerName,
+      config.gridMetadataBlobName, gridMetadataJSON, (error, result, response) => {
+      if(error) { this.errorCallback(error) }
+      else { /* gridMetadata uploaded */ }
+    })
+  }
+
+  download() {
+    if (!this.isContainerCreated) return
+
+    //TODO: dowload and return Grid object
+  }
+
+  delete() {
+    if (!this.isContainerCreated) return
+
+    this.blobService.deleteBlob(config.blobContainerName,
+      config.gridMapBlobName, (error, response) => {
+      if(error) { this.errorCallback(error) }
+      else { /* gridMap deleted */ }
+    })
+
+    this.blobService.deleteBlob(config.blobContainerName,
+      config.gridMetadataBlobName, (error, response) => {
+      if(error) { this.errorCallback(error) }
+      else { /* gridMetadata deleted */ }
+    })
+  }
+}
