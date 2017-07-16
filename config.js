8eb3bdd6ba51d4d0137bb57b633c8044183c2291
Serge Korzh <serge.korzh@gmail.com>
2017-07-16 10:49:15 +0300

    Add azure storage connection string

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   azureBlob: {
-    connectionString: process.env.connectionString
+    connectionString: process.env.CUSTOM_CONNSTR_azureStorage
       || 'BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;',
     blobContainerName: 'grid-container',
     gridMapBlobName: 'grid-map',
